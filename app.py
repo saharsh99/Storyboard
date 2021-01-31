cbf9b909f197d16ece2bc03d439a0b434408cd1c
Saharsh <saharshnigam99@gmail.com>
2021-02-01 01:45:42 +0530

    Registration Complete

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'storyboard'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
 Articles = Articles()
 
 @app.route('/')
@@ -25,21 +34,39 @@ def article(id):
     return render_template('article.html', id=id)
 
 class RegisterForm(Form):
-    name = StringField('Name',[validators.Length(min=1,max=50)])
-    username = StringField('Username', [validators.Length(min=4,max=25)])
-    email = StringField('Email',[validators.Length(min=6,max=50)])
-    password = PasswordField('Password',[
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm',message='Passwords do not match')
+        validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm Password')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
-    return render_template('register.html',form=form)
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
 
 if __name__ == '__main__':
+    app.secret_key='secretkey'
     app.run(debug=True)
